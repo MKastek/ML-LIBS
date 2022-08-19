423e50be9fff3a033835fa7de13eee03bfb0a303
MKastek <mkastek8@gmail.com>
2022-08-19 09:25:25 +0200

    new branch mlp

--- ml-libs.py
+++ ml-libs.py
@@ -16,59 +16,6 @@ from sklearn.neural_network import MLPRegressor, MLPClassifier
 import pickle as pk
 
 
-def WhittakerSmooth(x, w, lambda_, differences=1):
-    '''
-    Penalized least squares algorithm for background fitting
-
-    input
-        x: input data (i.e. chromatogram of spectrum)
-        w: binary masks (value of the mask is zero if a point belongs to peaks and one otherwise)
-        lambda_: parameter that can be adjusted by user. The larger lambda is,  the smoother the resulting background
-        differences: integer indicating the order of the difference of penalties
-
-    output
-        the fitted background vector
-    '''
-    X = np.matrix(x)
-    m = X.size
-    E = eye(m, format='csc')
-    for i in range(differences):
-        E = E[1:] - E[:-1]  # numpy.diff() does not work with sparse matrix. This is a workaround.
-    W = diags(w, 0, shape=(m, m))
-    A = csc_matrix(W + (lambda_ * E.T * E))
-    B = csc_matrix(W * X.T)
-    background = spsolve(A, B)
-    return np.array(background)
-
-
-def airPLS(x, lambda_=100, porder=1, itermax=15):
-    '''
-    Adaptive iteratively reweighted penalized least squares for baseline fitting
-
-    input
-        x: input data (i.e. chromatogram of spectrum)
-        lambda_: parameter that can be adjusted by user. The larger lambda is,  the smoother the resulting background, z
-        porder: adaptive iteratively reweighted penalized least squares for baseline fitting
-
-    output
-        the fitted background vector
-    '''
-    m = x.shape[0]
-    w = np.ones(m)
-    for i in range(1, itermax + 1):
-        z = WhittakerSmooth(x, w, lambda_, porder)
-        d = x - z
-        dssn = np.abs(d[d < 0].sum())
-        if (dssn < 0.001 * (abs(x)).sum() or i == itermax):
-            if (i == itermax): print('WARING max iteration reached!')
-            break
-        w[d >= 0] = 0  # d>0 means that this point is part of a peak, so its weight is set to 0 in order to ignore it
-        w[d < 0] = np.exp(i * np.abs(d[d < 0]) / dssn)
-        w[0] = np.exp(i * (d[d < 0]).max() / dssn)
-        w[-1] = w[0]
-    return z
-
-
 def read_data(filepath):
     return pd.read_csv(filepath)
 
@@ -87,10 +34,6 @@ def filter_noise(X, treshold):
     X_limited = X.iloc[:,X.columns.astype('float')<220]
     X = X.assign(noise_level = X_limited.apply(max, axis=1))
     return X[X['noise_level'] < treshold].drop(['noise_level'],axis=1)
-
-
-def correct_baseline(X):
-    return X.apply(lambda row : row-airPLS(row), axis = 1)
 
 
 def limit_wavelength(X,w_min = 225, w_max = 940):
@@ -120,7 +63,7 @@ def tune_xgb_model(X_train, y_train):
 
     xgb_grid = GridSearchCV(xgb,
                             parameters,
-                            cv=5,
+                            cv=10,
                             n_jobs=5,
                             verbose=True)
 
@@ -187,6 +130,8 @@ def report(y_test, y_pred, element, report_df):
     r2 = np.round(r2_score(y_test, y_pred), 3)
     mae = np.round(mean_absolute_error(y_test, y_pred), 3)
     report_df[element] = [r2, mae]
+    report_df['score'] = ['R2', 'MAE']
+    report_df = report_df.set_index('score')
     report_df.to_csv(os.path.join('report','xgboost_report.csv'))
     return r2, mae
 
@@ -230,18 +175,18 @@ def test(pca,element):
             xgb_model = load(os.path.join('saved-models',element,'xgboost_'+element+'.joblib'))
             predictions.append(xgb_model.predict(pca.transform(row.values.reshape(1, -1))))
 
-        predictions_df[target] = [np.average(predictions), 1.96*np.std(predictions)]
+        predictions_df[target] = [np.average(predictions), np.std(predictions)]
     predictions_df.to_csv(os.path.join('test',element,'test_'+element+'.csv'))
 
 
-def test_all(pca):
+def test_all():
+    pca = pk.load(open(os.path.join("saved-pca", "pca.pkl"), 'rb'))
     for element in ['Cr', 'Mn', 'Mo', 'Ni']:
         test(pca,element)
 
 
 if __name__ == "__main__":
     train()
-    pca = pk.load(open(os.path.join("saved-pca","pca.pkl"),'rb'))
-    test_all(pca)
+    test_all()
 #test(pca, element)
 
